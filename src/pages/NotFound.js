39858a9cbf05c7ed979be6cf491d9dab84f4170a
Zedtai <coquin.atthaphon@outlook.com>
2023-09-21 14:57:32 +0200

    create NotFound page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+//Error page
+export default function NotFound() {
+    return (
+        <div className="errorPage">
+            <h1>404 - Not Found</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+            <Link to="/">Accueil</Link>
+        </div>
+    );
+}
