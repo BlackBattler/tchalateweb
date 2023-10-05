f080023bec587677cfb118e90c4b849b74df5315
Battler <coquin.atthaphon@outlook.com>
2023-10-06 00:29:33 +0200

    scroll header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p className="footerText">
-        TCHALATE Copyright © 2023 - All Right Reserved
+        Copyright © 2023 Tchalate - All Right Reserved
       </p>
     </footer>
   );
