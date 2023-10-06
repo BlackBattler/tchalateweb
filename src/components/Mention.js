fdf64d57a08d7833a7334b74108326cb5d8cd12f
Battler <coquin.atthaphon@outlook.com>
2023-10-06 19:23:37 +0200

    ajout text mention legale

--- src/components/Mention.js
+++ src/components/Mention.js
@@ -5,6 +5,28 @@ export default function HomeContact() {
   return (
     <section className="sectionMention">
       <h1>Mention Légale</h1>
+      <ul>
+        <li>
+          <h2>Éditeur</h2>
+          <p>Tchalate SAS</p>
+          <p>Adresse :</p>
+          <p>44 Rue de Bonnieux</p>
+          <p>38500 Voiron</p>
+          <p>France</p>
+          <p>Numero siret :953 865 904 R.C.S. Grenoble</p>
+        </li>
+        <li>
+          <h2>Directeur de publication</h2>
+          <p>Phoomongchai-Coquin Atthaphon</p>
+        </li>
+        <li>
+          <h2>Hébergement</h2>
+          <p>OVH</p>
+          <p>Siège social : 2 rue Kellermann</p>
+          <p>59100 Roubaix</p>
+          <p>France</p>
+        </li>
+      </ul>
     </section>
   );
 }
